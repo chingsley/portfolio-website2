c88c0f7d2ef93d83a47b392459063fc1acfa9c08
Kingsley Eneja <kingsley.eneja@getir.com>
2023-02-15 19:28:41 +0100

    Implement the dashboard section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome To <br />
+          My Personal Portfolio
+        </SectionTitle>
+        <SectionText>
+          I am a full stack software engineer with a degree in computer engineering and over 5 years of professional experience. I specialise in architecting, designing and developing highly scalable software systems.
+        </SectionText>
+        <Button onClick={props.handleClick}>View Resume</Button>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
